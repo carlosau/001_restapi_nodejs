7a641c59ec5da67efacd6e4a6a9d9d71e5c89290
carlosau <carlosaugsz@hotmail.com>
2023-06-09 23:20:16 -0300

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const 
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+
+mongoose.
+connect('mongodb+srv://admin:IJeTXNRnbcOFBvnb@testnodeapi.vrrtugz.mongodb.net/NodeAPI_DB?retryWrites=true&w=majority')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, ()=> {
+        console.log('Node API app is running on port 3000')
+    })
+}).catch((error) => {
+    console.log(error)
+})
